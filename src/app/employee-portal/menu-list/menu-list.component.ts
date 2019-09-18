4c4421fa97ddd202b504d5d734e7a13d01e53146
Preeti Dasondhi <preeti.dasondhi@team.neustar>
2019-09-18 16:20:25 +0530

    add balance and tranaction-detail key in users json + selection table in menu list

--- src/app/employee-portal/menu-list/menu-list.component.ts
+++ src/app/employee-portal/menu-list/menu-list.component.ts
@@ -1,26 +1,16 @@
 import { Component, OnInit, ViewChild } from '@angular/core';
 import { AngularMaterialModule } from '../../shared/angular-material.module';
 import { UserService } from '../../userData.service';
-import {SelectionModel} from '@angular/cdk/collections';
-import {MatTableDataSource} from '@angular/material/table';
+import { SelectionModel } from '@angular/cdk/collections';
+import { MatTableDataSource } from '@angular/material/table';
 export interface PeriodicElement {
-  name: string;
-  position: number;
-  weight: number;
-  symbol: string;
+  item_id: number;
+  item_name: string;
+  quantity: number;
+  image_src: string;
+  price: number;
 }
-const ELEMENT_DATA = [
-  {position: 1, name: 'Hydrogen', weight: 1.0079, symbol: 'H'},
-  {position: 2, name: 'Helium', weight: 4.0026, symbol: 'He'},
-  {position: 3, name: 'Lithium', weight: 6.941, symbol: 'Li'},
-  {position: 4, name: 'Beryllium', weight: 9.0122, symbol: 'Be'},
-  {position: 5, name: 'Boron', weight: 10.811, symbol: 'B'},
-  {position: 6, name: 'Carbon', weight: 12.0107, symbol: 'C'},
-  {position: 7, name: 'Nitrogen', weight: 14.0067, symbol: 'N'},
-  {position: 8, name: 'Oxygen', weight: 15.9994, symbol: 'O'},
-  {position: 9, name: 'Fluorine', weight: 18.9984, symbol: 'F'},
-  {position: 10, name: 'Neon', weight: 20.1797, symbol: 'Ne'},
-];
+
 @Component({
   selector: 'app-menu-list',
   templateUrl: './menu-list.component.html',
@@ -28,28 +18,27 @@ const ELEMENT_DATA = [
 })
 export class MenuListComponent implements OnInit {
 
-   displayedColumns: string[] = ['position', 'name', 'weight', 'symbol'];
-    dataSource = (ELEMENT_DATA);
-  columns: string[] = ['item_id', 'item_name', 'quantity', 'price', 'image', 'actionColumn'];
- selection = new SelectionModel<PeriodicElement>(true, []);
+  displayedColumns: string[] = ['select','item_id', 'item_name', 'quantity', 'image', 'price'];
+  dataSource = new MatTableDataSource<PeriodicElement>([]);
+  selection = new SelectionModel<PeriodicElement>(true, []);
+  constructor(private userService: UserService) {
 
-    constructor(private userService: UserService) {
-
-    }
+  }
   ngOnInit() {
     const self = this;
     const initialSelection = [];
     const allowMultiSelect = true
-    // this.dataSource.sort = this.sort;
     this.userService.getItemList()
                     .subscribe((res) => {
-                      self.dataSource = res;
+                      //console.log(res);
+                      self.dataSource = new MatTableDataSource<PeriodicElement>(res);
                     });
   }
+
   /** Whether the number of selected elements matches the total number of rows. */
   isAllSelected() {
     const numSelected = this.selection.selected.length;
-    const numRows = this.dataSource.length;
+    const numRows = this.dataSource.data.length;
     return numSelected === numRows;
   }
 
@@ -57,7 +46,7 @@ export class MenuListComponent implements OnInit {
   masterToggle() {
     this.isAllSelected() ?
         this.selection.clear() :
-        this.dataSource.forEach(row => this.selection.select(row));
+        this.dataSource.data.forEach(row => this.selection.select(row));
   }
 
   /** The label for the checkbox on the passed row */
@@ -65,7 +54,10 @@ export class MenuListComponent implements OnInit {
     if (!row) {
       return `${this.isAllSelected() ? 'select' : 'deselect'} all`;
     }
-    return `${this.selection.isSelected(row) ? 'deselect' : 'select'} row ${row.position + 1}`;
+    return `${this.selection.isSelected(row) ? 'deselect' : 'select'} row ${row.item_id + 1}`;
   }
-
+  buyItem() {
+    console.log(this.selection['_selected']);
+    
+  }
 }
