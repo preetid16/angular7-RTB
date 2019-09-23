5e373509fd9518df34464e3070d9ddc98fae39f6
Pooja Rai <pooja.rai@impetus.co.in>
2019-09-23 13:51:10 +0530

    Added functionality to Add Employee for Admin

--- src/app/admin-portal/manage-employee/manage-employee.component.ts
+++ src/app/admin-portal/manage-employee/manage-employee.component.ts
@@ -1,9 +1,15 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Inject } from '@angular/core';
 import { MatDialog, MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
 import { UserService } from './../../userData.service';
 import { Employee } from "./../../shared/interface/employee.model";
 import { TitleCasePipe } from "./../../shared/pipe/titleCase.pipe";
 import { FormControl, FormGroup, Validators } from '@angular/forms';
+
+
+export interface DialogData {
+  animal: string;
+  name: string;
+}
 
 @Component({
   selector: 'app-manage-employee',
@@ -15,9 +21,13 @@ export class ManageEmployeeComponent implements OnInit {
   displayedColumns: string[] = ['name', 'empId', 'email', 'location', 'department', 'actionColumn'];
   public addForm: FormGroup;
   users: Employee[];
-  showAddForm : boolean = false;
-  showTable : boolean = true;
-  constructor(private service: UserService) { }
+  showAddForm: boolean = false;
+  showTable: boolean = true;
+
+  animal: string;
+  name: string;
+
+  constructor(private service: UserService, public dialog: MatDialog) { }
 
   ngOnInit() {
     this.service.getUsers()
@@ -25,22 +35,7 @@ export class ManageEmployeeComponent implements OnInit {
         this.users = data;
       });
 
-    this.addForm = new FormGroup({
-      name: new FormControl('', [Validators.required, Validators.maxLength(10)]),
-      username: new FormControl('', [Validators.required, Validators.maxLength(10)]),
-      empId: new FormControl('', [Validators.required, Validators.maxLength(6)]),
-      email: new FormControl('', [Validators.required, Validators.email]),
-      department: new FormControl('', [Validators.required]),
-      location: new FormControl('', [Validators.required]),
-      password: new FormControl('', [Validators.required]),
-      confirmPassword: new FormControl('', [Validators.required]),
-      is_admin: new FormControl('', [])
-    });
   }
-  public hasError = (controlName: string, errorName: string) => {
-    return this.addForm.controls[controlName].hasError(errorName);
-  }
-
   deleteEmployee(employee: Employee, event: Event) {
     let deleteBy = (employee.empId) ? employee.empId : employee.id;
     this.service.deleteUser(deleteBy)
@@ -49,9 +44,57 @@ export class ManageEmployeeComponent implements OnInit {
       });
   };
 
-  openDialog (){
-    // this.showAddForm = true;
-    // this.showTable = false;
+  openDialog() {
+    const dialogRef = this.dialog.open(DialogOverviewExampleDialog, {
+      width: '450px',
+      data: { name: this.name, animal: this.animal }
+    });
+
+    dialogRef.afterClosed().subscribe(result => {
+
+    });
+  }
+
+}
+
+@Component({
+  selector: 'dialog-overview-example-dialog',
+  templateUrl: './dialog-overview-example-dialog.html',
+})
+export class DialogOverviewExampleDialog implements OnInit {
+  public addForm: FormGroup;
+  constructor(
+    public dialogRef: MatDialogRef<DialogOverviewExampleDialog>,
+    @Inject(MAT_DIALOG_DATA) public data: DialogData,
+    private service: UserService, ) { }
+  ngOnInit() {
+    this.addForm = new FormGroup({
+      name: new FormControl('', [Validators.required, Validators.maxLength(10)]),
+      username: new FormControl('', [Validators.required, Validators.maxLength(10)]),
+      empId: new FormControl('', [Validators.required, Validators.maxLength(6)]),
+      email: new FormControl('', [Validators.required, Validators.email]),
+      department: new FormControl('', [Validators.required]),
+      location: new FormControl('', [Validators.required]),
+      password: new FormControl('', [])
+    });
+  }
+  onNoClick(): void {
+    this.dialogRef.close();
+  }
+
+  addNewUser(addFormObj) {
+    console.log(addFormObj);
+    if (addFormObj.password == '') {
+      addFormObj.password = addFormObj.username;
+    }
+    this.service.createUser(this.addForm.value)
+      .subscribe(data => {
+        swal('success', "Employee Details added Successfully.");
+      });
+  }
+
+  public hasError = (controlName: string, errorName: string) => {
+    return this.addForm.controls[controlName].hasError(errorName);
   }
 
 }
